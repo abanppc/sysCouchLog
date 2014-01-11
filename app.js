0a73849174a28203c5a629512a1dde765d980b79
andrewhodel <andrewhodel@gmail.com>
2014-01-11 13:13:16 -0600

    one too many params

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var server = require("./lib/node-listen-udp-syslog");
 var listen = {ip:'0.0.0.0',port:1514};
 
 // start server
-server.start(listen.ip, listen.port, function (msg, rinfo) {
+server.start(listen.ip, listen.port, function (msg) {
     console.log(msg);
 });
