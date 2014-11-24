b501a88b046f9769fc06487233a37e06e98298a8
jrad <behradz@gmail.com>
2014-11-24 16:28:09 +0330

    add forever preinstall script

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 var server = require("./lib/node-listen-udp-syslog");
 var cluster = require( 'cluster' );
 var config = require( './config' );
+var nano = require('nano');
 
-var nano   = require('nano')(
-    'http://'+config.couchdb.user+":"+config.couchdb.pass+"@"+config.couchdb.host+':' + config.couchdb.port );
-
+var db = nano(
+    'http://'+config.couchdb.user+":"+config.couchdb.pass+"@"+config.couchdb.host+':' + config.couchdb.port + "/" + config.couchdb.db
+);
 
 if( cluster.isMaster ) {
-    nano.db.create(config.couchdb.db, function (err1) {
-        nano.db.get(config.couchdb.db, function(err, body) {
+    /*db.create(config.couchdb.db, function (err1) {
+        db.get(config.couchdb.db, function(err, body) {
             if (err) {
                 console.error(err, err1);
                 process.exit(1);
@@ -18,21 +19,19 @@ if( cluster.isMaster ) {
                 }
             }
         });
-    });
+    });*/
 } else {
 
     process.on( 'uncaughtException', function(err){
         console.error( err );
     });
 
-    var db = nano.use( config.couchdb.db );
-
     server.start( config.ip, config.port, function (msg) {
-        //console.log( msg );
         var data = msg;
         try {
-            data = JSON.parse( msg.content.replace( /\w+:\s/g, '' ) )
+            data = JSON.parse( msg.content.replace( /\w+:\s/g, '' ).replace( /@cee:/g, '') );
         } catch( e ) {
+            console.error( e );
         }
         data.createdAt = Date.now();
         db.insert( data, function(err, body) {
